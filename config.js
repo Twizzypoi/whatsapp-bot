7458a31f907d1d2deb50c4af000e28b88f4756af
Twizzypoi <haterrrsa@gmail.com>
2024-10-25 01:54:02 +0200

    Update config.js

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ DEBUG =
 
 module.exports = {
   VERSION: "v1.2.8",
-  SESSION:
+  SESSION:'levanter_25fe156c90410941c8a6eb3c2d067efe16'
     process.env.ASENA_SESSION === undefined ? "" : process.env.ASENA_SESSION,
   EXT: process.env.EXT === undefined ? undefined : process.env.EXT,
   LANG:
